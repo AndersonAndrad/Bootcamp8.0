9d190275f0aed6f62cda40580636f89f0d81fe4e
AndersonAndrad <Anderson_Andrade_@outlook.com>
2019-10-26 11:55:15 -0300

    [Update] - Adding user register

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,30 @@
 // dependencies
+import * as Yup from 'yup';
 
 // model
 import User from '../models/User';
 
 class UserController {
   async store(req, res){
-    return res.json({status: 'User controller store is running '})
+    const schema = Yup.object().shape({
+      name: Yup.string().required().min(3),
+      email: Yup.string().required().email(),
+      password: Yup.string().required().min(6),
+      provider: Yup.boolean().required()
+    });
+
+    if(!(await schema.isValid(req.body))){
+      return res.json({Status: 'Error, check your information'});
+    }
+
+    const {name, email, password, provider} = await User.create(req.body);
+    
+    return res.json({
+      name,
+      email,
+      password,
+      provider
+    })
   }
 }
 
