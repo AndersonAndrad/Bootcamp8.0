2b229f886abca182627f406e32e83e287f39c007
AndersonAndrad <Anderson_Andrade_@outlook.com>
2019-11-17 21:44:16 -0300

    [Update] - adding appointment model

--- src/database/index.js
+++ src/database/index.js
@@ -9,7 +9,7 @@ import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appoitment';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor(){
