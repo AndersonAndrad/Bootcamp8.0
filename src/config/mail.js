714f11fe07e69a11255a7f9b77cb3746dae74207
AndersonAndrad <Anderson_Andrade_@outlook.com>
2019-12-16 20:38:34 -0300

    [Create] - Connection to email server

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "0f6fc5b0eab152",
+    pass: "792ecf49d067c4"
+  },
+  default:{
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+};
